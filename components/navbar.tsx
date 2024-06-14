d1794bbda039158ae21697d52e66a11cdc3127af
BoruTamena <borutamena46@gmail.com>
2024-06-14 10:31:09 -0400

    home page sekeleton implemented

---
+++ components/navbar.tsx
@@ -0,0 +1,30 @@
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="container mx-auto sticky top-0">
+        <nav className="p-4 bg-secondary-200 relative top-3" >
+            <ul className="flex justify-start  items-center space-x-5">
+
+                <li className="  text-secondary-950 ">
+                   <p className="font-sans capitalize">workspace</p>
+                </li>
+
+                <li className="text-secondary-950 flex-1">
+                    <p className="font-sans capitalize">create project </p>
+                </li>
+
+                <li className="text-secondary-950 ">
+                    <button className=" bg-primary-400 rounded-md capitalize p-2  text-secondary-100"> create New Project</button>
+                </li>
+                <li className="text-secondary-950 px-4 py-2 rounded-sm bg-secondary-500 ">
+                    B
+                </li>
+                
+            </ul>
+        </nav>
+    </div>
+  )
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,38 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors:{
+
+    'primary': {
+        '50': '#fdf6fd',
+        '100': '#f9edfa',
+        '200': '#f2d9f5',
+        '300': '#ebbbec',
+        '400': '#dc8add',
+        '500': '#cc68cd',
+        '600': '#b148b0',
+        '700': '#92398f',
+        '800': '#773174',
+        '900': '#632c5f',
+        '950': '#3f133c',
+    },
+  'secondary': {
+        '50': '#fef5f2',
+        '100': '#fee8e2',
+        '200': '#fdd6cb',
+        '300': '#fbb9a6',
+        '400': '#f79c82',
+        '500': '#ed6b46',
+        '600': '#d95029',
+        '700': '#b7401e',
+        '800': '#97381d',
+        '900': '#7e331e',
+        '950': '#44180b',
+    },
+    
+   
+
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
